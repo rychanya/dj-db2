8c7deb43fe0fdf9f9dbe7870e7162dbc8bdb7604
Dmitrii Rumyancev <rychanya@gmail.com>
2020-06-20 01:19:42 +0300

    task 3

--- orm_query/articles/views.py
+++ orm_query/articles/views.py
@@ -6,10 +6,9 @@ from .models import Article
 
 def articles_list(request):
     template_name = 'articles/news.html'
-    context = {}
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = '-published_at'
-
+    context = {
+        'object_list': Article.objects.order_by('-published_at')\
+            .defer('published_at')\
+            .select_related('genre', 'author').all()
+    }
     return render(request, template_name, context)
